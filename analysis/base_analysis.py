0b1788b6a594ba4dfefac28369d2b2daeedc297e
dyknapp <dknapp@princeton.edu>
2022-01-06 16:57:58 -0500

    Matching conditions

--- analysis/base_analysis.py
+++ analysis/base_analysis.py
@@ -19,9 +19,6 @@ import numpy as np
 from pathlib import Path
 import os
 
-wd = '/Genomics/ayroleslab2/scott/bees/bee-box/analysis'
-os.chdir(wd)
-
 
 # data_dir = "/Genomics/ayroleslab2/scott/long-timescale-behavior/data"
 
@@ -33,34 +30,36 @@ logging.basicConfig(
 logger = logging.getLogger("analysis_logger")
 
 import utils.trx_utils as trx_utils
-bee_tracks_df = pd.read_csv("../data/tracking_results/20210909_run000_00000000_48Hrs_libx264/20210909_run000_00000000_48Hrs_libx264_000/20210909_run000_00000000_48Hrs_libx264_000_aruco_data_with_track_numbers.csv")
+bee_tracks_df = pd.read_csv("d:\\bee_test\\files\\test_aruco_data_with_track_numbers.csv")
 
 # %%
 bee_tracks_df = bee_tracks_df[["Frame", "Tag", "abdomenX","abdomenY"]]
+print(bee_tracks_df)
 bee_tracks_df_x = bee_tracks_df[bee_tracks_df.Tag != -1].pivot(index='Frame', columns='Tag', values='abdomenX')
 bee_tracks_df_y = bee_tracks_df[bee_tracks_df.Tag != -1].pivot(index='Frame', columns='Tag', values='abdomenY')
 # %%
 bee_tracks = np.stack((bee_tracks_df_x.to_numpy(), bee_tracks_df_y.to_numpy()),axis=1)
 bee_tracks[:,1,:] = -bee_tracks[:,1,:]
 # %%
-bee_tracks
 missing_ct = np.count_nonzero(np.isnan(bee_tracks[:,0,:]),axis=0)
 missing_freq = missing_ct/bee_tracks.shape[0]
 bee_tracks = bee_tracks[:,:,missing_freq < 0.5]
+print(bee_tracks)
 # %%
 frame_count, node_count,instance_count = bee_tracks.shape
 bee_tracks = trx_utils.fill_missing(bee_tracks, kind="linear")
+
 bee_vel = trx_utils.instance_node_velocities_bees(bee_tracks,0,bee_tracks.shape[0])
 mask = np.stack(((bee_vel > 100),(bee_vel > 100)), axis=1)
 bee_tracks[mask] = np.nan
 
-bee_tracks = trx_utils.fill_missing(bee_tracks, kind="linear")
+# bee_tracks = trx_utils.fill_missing(bee_tracks, kind="linear")
 bee_tracks = trx_utils.smooth_median(bee_tracks,window=5)
 # bee_tracks = trx_utils.smooth_ewma(bee_tracks,alpha=0.3)
 
 
 start_frame = 0
-end_frame = int(20*60)
+end_frame = int(20 * 60)
 for bee_id in range(instance_count):
     plt.plot(bee_tracks[start_frame:end_frame,0,bee_id],bee_tracks[start_frame:end_frame,1,bee_id],alpha = 0.1)
 # %%
@@ -74,7 +73,7 @@ from cv2 import cv2
 from matplotlib.animation import FuncAnimation
 import palettable
 
-cap = cv2.VideoCapture("/Genomics/ayroleslab2/scott/bees/bee-box/data/tracking_results/20210909_run000_00000000_48Hrs_libx264/20210909_run000_00000000_48Hrs_libx264_000/20210909_run000_00000000_48Hrs_libx264_000_annotated.mp4")
+cap = cv2.VideoCapture("d:\\bee_test\\test.mp4")
 
 
 colors = distinctipy.get_colors(instance_count)
@@ -83,12 +82,14 @@ colors = distinctipy.get_colors(instance_count)
 # frame_numbers = list(range(start_frame, end_frame))
 def animate(i):
     plt.cla()
-    frame_idx =  i + 5000
+    frame_idx =  i
 
     print(f"Saving frame {i}")
     for bee_idx in range(instance_count):
         ax.axis("off")
         ax.set_axis_off()
+        ax.set_xlim([0, 3660])
+        ax.set_ylim([0, 3660])
         fig.add_axes(ax)
         fig.patch.set_visible(False)
         data = bee_tracks[:,:,bee_idx]
@@ -105,12 +106,13 @@ def animate(i):
                 color=colors[bee_idx],
                 alpha=1
             )
-    cap.set(cv2.CAP_PROP_POS_FRAMES,frame_idx + 5)
-    res, frame = cap.read()
-    frame = frame[:, :, 0]
-    plt.imshow(frame, cmap="gray", zorder=0)
-
-
+    # cap.set(cv2.CAP_PROP_POS_FRAMES,frame_idx + 5)
+    # res, frame = cap.read()
+    # if res:
+    #     frame = frame[:, :, 0]
+    #     plt.imshow(frame, cmap="gray", zorder=0)
+    # else:
+    #     raise ValueError("Video error")
 
 fig, ax = plt.subplots()
 # ax = plt.Axes(fig, [0., 0., 1., 1.])
@@ -123,9 +125,9 @@ fig, ax = plt.subplots()
 # fig.patch.set_visible(False)
 # fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
 
-plt.rcParams['animation.ffmpeg_path'] = '/Genomics/argo/users/swwolf/.conda/envs/sleap_dev/bin/ffmpeg'
+plt.rcParams['animation.ffmpeg_path'] = 'D:\\ffmpeg-4.4-essentials_build\\ffmpeg-4.4-essentials_build\\bin\\ffmpeg.exe'
 ani = FuncAnimation(fig, animate, frames=range(100,600)).save(
-    "withframe.mp4",
+    "d:\\bee_test\\withframe.mp4",
     fps=20,
     bitrate=20000,
     dpi=300,
